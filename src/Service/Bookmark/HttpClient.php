f0ec6c37d1aa67512af64365ceecdc284d37ce08
Andrey Ilynov <ilynov.andrey@gmail.com>
2020-04-27 20:19:05 +0300

    Remove comments

--- src/Service/Bookmark/HttpClient.php
+++ src/Service/Bookmark/HttpClient.php
@@ -11,9 +11,6 @@ class HttpClient
         $client = new \GuzzleHttp\Client();
         $response = $client->request('GET', $url);
 
-//        $response->getStatusCode(); // 200
-//        $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
-
-        return (string) $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+        return (string) $response->getBody();
     }
 }
